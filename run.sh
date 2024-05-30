9fb728a0de5a603608daeb73d3077492bb6ae909
Hoehne Johannes <johannes.hoehne@metrosystems.net>
2024-05-30 14:36:52 +0200

    piped run script a bit

--- run.sh
+++ run.sh
@@ -2,15 +2,25 @@
 
 docker run --rm -d -p 5432:5432 -e POSTGRES_PASSWORD="1234" --name postgres postgres:alpine
 sleep 2
+
 docker run \
  --rm \
+ -d \
  -p 8080:8080 \
  -p 8088:8088  \
  -e ODJ_DEP_ADDRESS_BOOK_API_URI=http://localhost:8080  \
  -e ODJ_DEP_POSTGRESQL_URL=postgresql://postgres:1234@host.docker.internal:5432/postgres \
  -e ODJ_DEP_USER_NAME='Hans Hubert' \
  --name addressbook \
- hoehne/addressbook:1.0 
+ hoehne/addressbook:2.0
+
+open "http://localhost:8088"
+while :
+do
+  sleep 10 &
+  wait $!
+  echo "Sleep over"
+done
 
 docker kill addressbook
 docker kill postgres
